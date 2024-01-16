5b4beb363fcb7e4c16c1fb093d9996b22be4a9db
Niklas Marion <niklas.marion@gmx.de>
2024-01-16 15:11:13 +0100

    only run workflow on dir change

--- frontend/src/components/OutsideClickHandler.tsx
+++ frontend/src/components/OutsideClickHandler.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 
 function OutsideClickHandler({ children, onOutsideClick = () => {} }: any) {
   const wrapperRef = useRef<HTMLDivElement>(null);
