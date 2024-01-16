cdcb0b4ce0238270101c9aeaa2477b20a1603c6b
Niklas Marion <niklas.marion@gmx.de>
2024-01-16 15:05:37 +0100

    add missing migration

---
+++ backend/prisma/migrations/20240116140459_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "date" DATETIME NOT NULL
+);
